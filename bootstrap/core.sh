8ce00682c7f54c6ef438ebb66be9c791620629a0
Kumar Rahul <blueraaga@gmail.com>
2019-08-14 13:30:22 +0200

    Added code for docker installation

--- bootstrap/core.sh
+++ bootstrap/core.sh
@@ -5,4 +5,47 @@
 
 echo -e "${YELLOW}------ Initiating Core Script ------${NC}"
 
+
+### Install Docker CE engine
+echo -e "${YELLOW}------ Install Docker ------${NC}"
+
+## Based on the instructions provided at
+## https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+## Uninstall old versions if present (safeguard)
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+## Set up the repository
+
+# Install packages to allow apt to use a repository over HTTPS
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Verify that you now have the key
+sudo apt-key fingerprint 0EBFCD88
+
+# Add the stable repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+## Install it now
+
+# Update package index
+sudo apt-get update
+
+# Install the latest version of Docker Engine - Community and containerd
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+## Verify that Docker Engine - Community is installed correctly
+sudo docker run hello-world
+
 echo -e "${YELLOW}------ Completed Core Script ------${NC}"
